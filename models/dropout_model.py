29f20aa71c99236e1069259e5b65acec90793059
kefirski <olavursky@gmail.com>
2017-09-29 00:56:36 +0300

    formation fix

--- models/dropout_model.py
+++ models/dropout_model.py
@@ -1,32 +1,31 @@
-import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class DropoutModel(nn.Module):
     def __init__(self):
-		super(DropoutModel, self).__init__()
+        super(DropoutModel, self).__init__()
 
-		self.fc = nn.ModuleList([
-			nn.Linear(784, 500),
-			nn.Linear(500, 400),
-			nn.Linear(400, 100),
-			nn.Linear(100, 10)
-		])
-	
-	def forward(self, input, p=0):
-		"""
+        self.fc = nn.ModuleList([
+            nn.Linear(784, 500),
+            nn.Linear(500, 400),
+            nn.Linear(400, 100),
+            nn.Linear(100, 10)
+        ])
+
+    def forward(self, input, p=0):
+        """
         :param input: An float tensor with shape of [batch_size, 784]
-		:param p: An float value in [0, 1.] with probability of elements to be zeroed
-		:return: An float tensor with shape of [batch_size, 10] filled with logits of likelihood
+        :param p: An float value in [0, 1.] with probability of elements to be zeroed
+        :return: An float tensor with shape of [batch_size, 10] filled with logits of likelihood
         """
 
-		result = input
+        result = input
 
-		for i, layer in enumerate(self.fc):
-			result = F.elu(layer(input))
+        for i, layer in enumerate(self.fc):
+            result = F.elu(layer(input))
 
-			if i < len(self.fc) - 1:
-				result = F.dropout(result, p, training=True)
+            if i < len(self.fc) - 1:
+                result = F.dropout(result, p, training=True)
 
-		return result
+        return result
--- models/simple_model.py
+++ models/simple_model.py
@@ -4,22 +4,22 @@ import torch.nn as nn
 
 class SimpleModel(nn.Module):
     def __init__(self):
-		super(SimpleModel, self).__init__()
+        super(SimpleModel, self).__init__()
 
-		self.fc = nn.Sequential(
-			nn.Linear(784, 500),
-			nn.ELU(),
-			nn.Linear(500, 400),
-			nn.ELU(),
-			nn.Linear(400, 100),
-			nn.ELU(),
-			nn.Linear(100, 10)
-		)
-	
-	def forward(self, input):
-		"""
+        self.fc = nn.Sequential(
+            nn.Linear(784, 500),
+            nn.ELU(),
+            nn.Linear(500, 400),
+            nn.ELU(),
+            nn.Linear(400, 100),
+            nn.ELU(),
+            nn.Linear(100, 10)
+        )
+
+    def forward(self, input):
+        """
         :param input: An float tensor with shape of [batch_size, 784]
-		:return: An float tensor with shape of [batch_size, 10] filled with logits of likelihood
+        :return: An float tensor with shape of [batch_size, 10] filled with logits of likelihood
         """
 
-		return self.fc(input)
+        return self.fc(input)
